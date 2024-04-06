173e96ceeca5a9d7b662b56ad7af250fe20afb02
Moibe <moi_estrello@hotmail.com>
2024-04-05 19:47:36 -0600

    Fix compiler

--- compiler.py
+++ compiler.py
@@ -1,14 +1,14 @@
 from cryptography.fernet import Fernet
 import nycklar.nodes as nodes
 
-def do():
+def do(userfile):
 
     key = Fernet.generate_key()
     fernet = Fernet(key)
 
     string_original = "oldball182ls"
     string_encriptado = fernet.encrypt(string_original.encode("utf-8"))
-    string_desencriptado = fernet.decrypt(nodes.user).decode("utf-8")
+    string_desencriptado = fernet.decrypt(userfile).decode("utf-8")
 
     print(f"String original: {string_original}")
     print(f"String encriptado: {string_encriptado}")
