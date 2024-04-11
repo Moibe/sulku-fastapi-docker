19cc9ea284a43c62fb71ff5309d7c75aae59c06c
Moibe <moi_estrello@hotmail.com>
2024-04-11 13:51:02 -0600

    Ready for prod

--- app.py
+++ app.py
@@ -3,35 +3,34 @@ import avaimet
 import gradio as gr
 
 def getAccess(userfile):
-        
-    sshListo, sftpListo = avaimet.conecta()
 
+    #Genera conexión inicial.       
+    sshListo, sftpListo = avaimet.conecta()
     #Obtiene la caja donde está guardados los tokens.
     caja = avaimet.obtenCaja(userfile)
-
     #Obtiene los tokens que hay en esa caja.
     tokens = avaimet.obtenTokens(sftpListo, caja)
-    
+    #Cierra la conexión.    
     avaimet.cierraConexion(sshListo, sftpListo)
     
     return tokens
 
 def debitToken(userfile):
 
+    #Genera conexión inicial.
     sshListo, sftpListo = avaimet.conecta()
-
     #Obtiene la caja donde está guardados los tokens.
     caja = avaimet.obtenCaja(userfile)
-
     #Obtiene los tokens que hay en esa caja.
     tokens = avaimet.obtenTokens(sftpListo, caja)
-
+    #Aplica las reglas de ésta app para debitar lo correspondiente.
     resultado_debitado = avaimet.aplicaReglas(sftpListo, caja, tokens)
-
+    #Cierra la conexión.  
     avaimet.cierraConexion(sshListo, sftpListo)
 
     return resultado_debitado
 
+#Interfaz Gráfica
 with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column():
@@ -42,6 +41,6 @@ with gr.Blocks() as demo:
             text_output = gr.Textbox()
 
     access_btn.click(fn=getAccess, inputs=text_input, outputs=text_output, api_name="getTokens")
-    debit_btn.click(fn=debitToken, inputs=text_input, outputs=text_output, api_name="print")
+    debit_btn.click(fn=debitToken, inputs=text_input, outputs=text_output, api_name="debitTokens")
 
 demo.launch()
