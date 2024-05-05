88c4a76eec0abf403ea4f9e55e399a14abf6e696
Moibe <moi.estrello@gmail.com>
2024-05-05 00:31:43 -0600

    Add path and query parameters

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import funciones
 app = FastAPI()
 
 @app.get("/")
-def hello(): 
+def start(): 
 
     return {"Status":"Deployed"}
 
@@ -14,8 +14,20 @@ def getTokens(userfile: str):
     print("Tipo de resultado:", type(tokens))
     return tokens
 
+@app.get("/getTokensQ/")
+def getTokens(userfile: str = "gAAAAABmEZA4SLBC2YczouOrjIEi9WNCNGOIvyUcqBUnzxNsftXTdy54KaX9x8mAjFkABSI6FJrdZDQKk_5lpJOgJoMChxlniw=="):
+    tokens = funciones.getAccess(userfile)
+    print("Tipo de resultado:", type(tokens))
+    return tokens
+
 @app.get("/debitTokens/{userfile}/{work}")
 def debitTokens(userfile: str, work: str):
+    tokens = funciones.debitTokens(userfile,work) 
+    print("Tipo de resultado:", type(tokens))
+    return tokens
+
+@app.get("/debitTokensQ/")
+def debitTokens(userfile: str, work: str = "picswap"):
     tokens = funciones.debitTokens(userfile,work) 
     print("Tipo de resultado:", type(tokens))
     return tokens
