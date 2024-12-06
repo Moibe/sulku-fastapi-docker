52a446ec7b7035350740b7510885721061a85418
Moibe <Moibe@users.noreply.huggingface.co>
2024-12-06 05:26:00 +0000

    Fish Nodes 🐟

---
+++ nycklar/nodes.py
@@ -0,0 +1,5 @@
+master = "root"
+realm = "165.22.53.200"
+key=b'kiSHsm7Y0hbyNdcFIav6OMT39gi29nIzT-rCrr0Tyc8=' #importante conn con server no es decompiler.
+users_data = "/chips/sulkusers-data/"
+users_credits = "/chips/sulkusers-credits/"
