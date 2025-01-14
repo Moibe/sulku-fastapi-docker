d9deb6c74a9d646aa2538110da85119e4e5b6b64
Moibe <moi_estrello@hotmail.com>
2025-01-14 01:47:40 -0600

    Rename Origins

--- nycklar/nodes.py
+++ nycklar/nodes.py
@@ -1,6 +1,6 @@
 master = "root"
 realm = "165.22.53.200"
 key=b'kiSHsm7Y0hbyNdcFIav6OMT39gi29nIzT-rCrr0Tyc8=' #importante conn con server no es decompiler.
-users_data = "chips/sulkusers-data/"
-users_credits = "chips/sulkusers-credits/"
+users_data = "chips/sulkusers_data/"
+users_credits = "chips/sulkusers_credits/"
 quota = "chips/sulku-quota"
